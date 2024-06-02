d6e032500af588d3b0a5518334aaaeb49b6d7ce7
Taya Mudrova <mudrova.taisiia@gmail.com>
2024-06-02 22:04:37 +0300

    added background color to accordion elements

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,9 +1,59 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
-const accordionContainerFaq = document.querySelector(
-  '.accordion-container.faq-list'
-);
-const accordionFaq = new Accordion(accordionContainerFaq, {});
+// const accordionContainerFaq = document.querySelector(
+//   '.accordion-container.faq-list'
+// );
+// const accordionFaq = new Accordion(accordionContainerFaq, {});
 
-accordionFaq;
+// document.addEventListener('DOMContentLoaded', function () {
+//   const accordionContainerFaq = document.querySelector(
+//     '.accordion-container.faq-list'
+//   );
+//   const accordionItems = accordionContainerFaq.querySelectorAll('.faq-item.ac');
+
+//   if (accordionItems.length > 0) {
+//     const firstItem = accordionItems[0];
+//     firstItem.classList.add('active');
+//     firstItem.querySelector('.faq-text-box.ac-panel').style.display = 'block';
+//     firstItem.querySelector('.faq-svg-down').style.transform = 'rotate(180deg)';
+//   }
+
+//   const accordionFaq = new Accordion(accordionContainerFaq, {
+//     onOpen: function (currentElement) {
+//       currentElement.classList.add('active');
+//     },
+//     onClose: function (currentElement) {
+//       currentElement.classList.remove('active');
+//     },
+//   });
+
+//   if (accordionFaq.items.length > 0) {
+//     accordionFaq.open(accordionFaq.items[0]);
+//   }
+
+//   accordionFaq;
+// });
+
+document.addEventListener('DOMContentLoaded', function () {
+  const accordionContainerFaq = document.querySelector(
+    '.accordion-container.faq-list'
+  );
+  const accordionItems = accordionContainerFaq.querySelectorAll('.faq-item.ac');
+
+  if (accordionItems.length > 0) {
+    const firstItem = accordionItems[0];
+    firstItem.classList.add('active');
+    firstItem.querySelector('.faq-text-box.ac-panel').style.display = 'block';
+    firstItem.querySelector('.faq-svg').style.transform = 'rotate(180deg)';
+  }
+
+  const accordionFaq = new Accordion(accordionContainerFaq, {
+    onOpen: function (currentElement) {
+      currentElement.classList.add('active');
+    },
+    onClose: function (currentElement) {
+      currentElement.classList.remove('active');
+    },
+  });
+});
