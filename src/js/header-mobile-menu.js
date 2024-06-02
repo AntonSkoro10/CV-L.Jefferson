ded341f5a7b2b8989d2607e8633c936e97548e77
AntonSkoro10 <antonskoro112@gmail.com>
2024-06-02 22:16:53 +0300

    update css and js

--- src/js/header-mobile-menu.js
+++ src/js/header-mobile-menu.js
@@ -1,20 +1,32 @@
-const mobileMenu = document.querySelector('.mobile-menu')
-const openMobileMenu = document.querySelector('.open-mobile-menu')
-const closeMobileMenu = document.querySelector('.close-btn-menu')
+const mobileMenu = document.querySelector('.mobile-menu');
+    const openMobileMenu = document.querySelector('.open-mobile-menu');
+    const closeMobileMenu = document.querySelector('.close-btn-menu');
+    const mobileMenuLinks = document.querySelectorAll('.mob-menu-link');
 
-openMobileMenu.addEventListener('click', () => {
-    mobileMenu.classList.add('is-open')
-    mobileMenu.classList.remove('is-closed')
-})
+    if (mobileMenu && openMobileMenu && closeMobileMenu) {
+        openMobileMenu.addEventListener('click', () => {
+            mobileMenu.classList.add('is-open');
+            mobileMenu.classList.remove('is-closed');
+        });
 
-closeMobileMenu.addEventListener('click', () => {
-    mobileMenu.classList.add('is-closed')
-    mobileMenu.classList.remove('is-open')
-})
+        closeMobileMenu.addEventListener('click', () => {
+            mobileMenu.classList.add('is-closed');
+            mobileMenu.classList.remove('is-open');
+        });
 
-window.addEventListener('click', function (event) {
-    if (!mobileMenu.contains(event.target) && !openMobileMenu.contains(event.target)) {
-        mobileMenu.classList.add('is-closed');
-        mobileMenu.classList.remove('is-open');
+        window.addEventListener('click', function (event) {
+            if (!mobileMenu.contains(event.target) && !openMobileMenu.contains(event.target)) {
+                mobileMenu.classList.add('is-closed');
+                mobileMenu.classList.remove('is-open');
+            }
+        });
+
+        mobileMenuLinks.forEach(link => {
+            link.addEventListener('click', () => {
+                mobileMenu.classList.add('is-closed');
+                mobileMenu.classList.remove('is-open');
+            });
+        });
+    } else {
+        console.error('Не знайдено всі необхідні елементи');
     }
-})
